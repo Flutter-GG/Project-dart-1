b209b2cad5ebe829810eb663ba4e8019caab40a9
Wejdan <Wejdantaltaibi@gmail.com>
2023-07-28 19:22:58 +0300

    added the data set and add new book almost done

--- bin/first_project.dart
+++ bin/first_project.dart
@@ -1,5 +1,94 @@
+import 'dart:io';
 import 'package:first_project/first_project.dart' as first_project;
+import 'package:first_project/book_data.dart';
 
 void main(List<String> arguments) {
-  print('Hello world: ${first_project.calculate()}!');
+  var count = 0;
+  while (count < 10) {
+    print("Welcome to Wejdan Library!");
+    print("please select from menu:");
+    print(
+        "1: View the available books, 2: Add new book, 3: Delete book, 4: Make book purchases, Q: quite.");
+    String selected = stdin.readLineSync()!;
+
+    switch (selected) {
+      case "1":
+        print("object");
+        break;
+
+      case "2":
+        addNewBook();
+        break;
+
+      case "3":
+        print("3 in progress");
+        break;
+
+      case "4":
+        print(" 4 in progress");
+        break;
+
+      case "Q" || "q":
+        exit(0);
+
+      default:
+        print("Wrong choese!");
+    }
+    count++;
+  }
+}
+
+addNewBook() {
+  try {
+    print("please inter book title:");
+    String? bookTitle = stdin.readLineSync()!;
+    print("please inter author name:");
+    String? authorName = stdin.readLineSync()!;
+    print("please inter brief Description about the book:");
+    String? briefDescription = stdin.readLineSync()!;
+    print("please inter publication date:");
+    String? publicationDate = stdin.readLineSync()!;
+    print("please inter book category:");
+    String? category = stdin.readLineSync()!;
+    print("please inter available copies of the book:");
+    int availableCopies = int.parse(stdin.readLineSync()!);
+    print("please inter the book price:");
+    int price = int.parse(stdin.readLineSync()!);
+
+    final bookMap = {
+      "bookTitle": bookTitle,
+      "authorName": authorName,
+      "briefDescription": briefDescription,
+      "publicationDate": publicationDate,
+      "category": category,
+      "availableCopies": availableCopies,
+      "price": price
+    };
+    myData.add(bookMap);
+  } catch (error) {
+    print("error");
+  }
+}
+
+class Book {
+  late String? bookTitle;
+  late String? authorName;
+  late String? briefDescription;
+  late String? publicationDate;
+  late String? category;
+  late int? availableCopies;
+  late String? price;
+
+  Book(
+      {this.bookTitle,
+      this.authorName,
+      this.briefDescription,
+      this.publicationDate,
+      this.category,
+      this.availableCopies,
+      this.price});
+  displayData() {
+    print(
+        "Book Title:$bookTitle Author Name: $authorName Brief Description: $briefDescription Publication Date: $publicationDate Category: $category Available Copies: $availableCopies Price: $price");
+  }
 }
---
+++ lib/book_data.dart
@@ -0,0 +1,101 @@
+final List myData = [
+  {
+    "Book_Title": "The Blue Elephant",
+    "Author Name": "Ahmed Mourad",
+    "Brief Description":
+        "A novel narrating the story of a psychiatrist dealing with amysterious case at a mental hospital.",
+    "PublicationDate": "2014",
+    "Category": "Mystery/Thriller",
+    "AvailableCopies": 5,
+    "Price": 10,
+  },
+  {
+    "Book_Title": "TheHandmaid's Tale",
+    "Author Name": "Margaret Atwood",
+    "Brief Description":
+        "A dystopian novel depicting a future characterized byoppression and persecution.",
+    "PublicationDate": "1985",
+    "Category": "Science Fiction/Dystopian",
+    "AvailableCopies": 3,
+    "Price": 8,
+  },
+  {
+    "Book_Title": "The Invisible Man",
+    "Author Name": "Mohamed Reda",
+    "Brief Description":
+        "A collection of short stories addressing identity and existence",
+    "PublicationDate": "2021",
+    "Category": "Short Stories/Fiction",
+    "AvailableCopies": 8,
+    "Price": 12,
+  },
+  {
+    "Book_Title": "Memory of the Body",
+    "Author Name": "Ahlam Mosteghanemi",
+    "Brief Description":
+        "A novel revolving around a woman living between the past and the present.",
+    "PublicationDate": "1993",
+    "Category": "Fiction/Romance",
+    "AvailableCopies": 2,
+    "Price": 7,
+  },
+  {
+    "Book_Title": "The Alchemist",
+    "Author Name": "Paulo Coelho",
+    "Brief Description":
+        "A symbolic novel shedding light on the journey in search of true treasure.",
+    "PublicationDate": "1988",
+    "Category": "Fiction/Self-Help",
+    "AvailableCopies": 6,
+    "Price": 9,
+  },
+  {
+    "Book_Title": "A Brief History of Time",
+    "Author Name": "Stephen Hawking",
+    "Brief Description":
+        "A scientific book explaining the theory of the Big Bang and the evolution of the universe.",
+    "PublicationDate": "1998",
+    "Category": "NonFiction/Science",
+    "AvailableCopies": 4,
+    "Price": 15,
+  },
+  {
+    "Book_Title": "Nineteen Minutes",
+    "Author Name": "Nineteen Minutes",
+    "Brief Description": "A novel exploring themes of love, fate, and destiny.",
+    "PublicationDate": "2004",
+    "Category": "Fiction/Drama",
+    "AvailableCopies": 7,
+    "Price": 14,
+  },
+  {
+    "Book_Title": "Pride and Prejudice",
+    "Author Name": "Jane Austen",
+    "Brief Description":
+        "A classic romance novel depicting societal norms and prejudices.",
+    "PublicationDate": "1813",
+    "Category": "Fiction/Romance",
+    "AvailableCopies": 10,
+    "Price": 5,
+  },
+  {
+    "Book_Title": "1984",
+    "Author Name": "George Orwell",
+    "Brief Description":
+        "A dystopian novel describing a totalitarian regime's control over society.",
+    "PublicationDate": "1949",
+    "Category": "Science Fiction/Dystopian",
+    "AvailableCopies": 9,
+    "Price": 6,
+  },
+  {
+    "Book_Title": "To Kill a Mockingbird",
+    "Author Name": "Harper Lee",
+    "Brief Description":
+        "A coming-of-age novel addressing racial injustice in the American South.",
+    "PublicationDate": "1960",
+    "Category": "Fiction/Drama",
+    "AvailableCopies": 1,
+    "Price": 11,
+  },
+];
