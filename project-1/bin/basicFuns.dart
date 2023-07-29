535f1be890b09e0c6ef3bb5081553c28f9a86b5d
Lama <84765301+Lama-Aldhafeeri@users.noreply.github.com>
2023-07-29 22:03:17 +0300

    Lama Aldhafeeri

--- project-1/bin/basicFuns.dart
+++ project-1/bin/basicFuns.dart
@@ -53,17 +53,39 @@ loadDateToList() {
   return booksListLocal;
 }
 
-/// Display distinct category
+/// Display distinct category 
 displayCategorys() {
   Set categorySet = {};
+  bool flag = false;
 
-  // use Set to print distinct category
+  // use Set to print distinct category 
   for (var book in booksList) {
     categorySet.add(book.category);
   }
   for (var category in categorySet) {
     print(category);
   }
+  do {
+    try {
+      print("Choes one of the category to display books related (e for exit)");
+      String userCategory = stdin.readLineSync()!;
+
+      if (userCategory == "e" || userCategory == "E") {
+        break;
+      }
+      for (var book in booksList) {
+        if (book.category == userCategory) {
+          flag = true;
+          book.display();
+        }
+      }
+      if (flag == false) {
+        print("Sorry the category you looking for does not exist");
+      }
+    } catch (error) {
+      print("please make sure you entered correct data");
+    }
+  } while (true);
 }
 
 /// display books if the title or author or category are exists
