a606b5cd73f4c6e1ebf07bddb1d55ce998fed277
Wejdan <Wejdantaltaibi@gmail.com>
2023-07-30 00:06:51 +0300

    Query Books && Display all Book Categories

--- bin/first_project.dart
+++ bin/first_project.dart
@@ -7,24 +7,92 @@ void main(List<String> arguments) {
     print("Welcome to Wejdan Library!");
     print("please select from menu:");
     print(
-        "1: View the available books, 2: Add new book, 3: Delete book, 4: Make book purchases, Q: quite.");
+        "1: Query about books, 2: Update the library , 3: Make book purchases, Q: quite.");
     String selected = stdin.readLineSync()!;
-
     switch (selected) {
       case "1":
-        displayAvailableBook();
+        print(
+            "1: To view all books, 2: To search in books, 3: Display all Book Categories, Q: quite.");
+        String? selected = stdin.readLineSync();
+        switch (selected) {
+          case '1':
+            displayAvailableBook();
+            break;
+          case '2':
+            print(
+                "1: To search books by title, 2: To search books by author, 3: To search books by category, Q: quite.");
+            String? selected = stdin.readLineSync();
+            switch (selected) {
+              case '1':
+                searchByTitle();
+                break;
+              case '2':
+                searchByAuthor();
+                break;
+              case '3':
+                searchByCategory();
+                break;
+              case 'q' || 'Q':
+                break;
+            }
+            break;
+          case '3':
+            print("please select the category from menu:");
+            print(
+                "1: Mystery/Thriller, 2: Science Fiction/Dystopian, 3: Short Stories/Fiction, 4:Fiction/Romance, 5: Fiction/Self-Help, 6: NonFiction/Science, 7: Fiction/Drama,  Q: quite.");
+            String? selected = stdin.readLineSync();
+            switch (selected) {
+              case '1':
+                searchInCategory('Mystery/Thriller');
+                break;
+              case '2':
+                searchInCategory('Science Fiction/Dystopian');
+                break;
+              case '3':
+                searchInCategory('Short Stories/Fiction');
+                break;
+              case '4':
+                searchInCategory('Fiction/Romance');
+                break;
+              case '5':
+                searchInCategory('Fiction/Self-Help');
+                break;
+              case '6':
+                searchInCategory('NonFiction/Science');
+                break;
+              case '7':
+                searchInCategory('Fiction/Drama');
+                break;
+              case 'q' || 'Q':
+                break;
+            }
+            break;
+          case 'q' || 'Q':
+            break;
+        }
         break;
 
       case "2":
-        addNewBook();
+        print(
+            "1: To add new book, 2: To delete book, 3: To update book info, Q: quite.");
+        String? selected = stdin.readLineSync();
+        switch (selected) {
+          case '1':
+            addNewBook();
+            break;
+          case '2':
+            deleteBook();
+            break;
+          case '3':
+            // TODO To update book info
+            break;
+          case 'q' || 'Q':
+            break;
+        }
         break;
 
       case "3":
-        deleteBook();
-        break;
-
-      case "4":
-        print("$myData[0]['briefDescription']");
+        // TODO Make book purchases && detailed invoice after the purchase
         break;
 
       case "Q" || "q":
@@ -66,7 +134,7 @@ addNewBook() {
     myData.insert(0, bookMap);
     print("The book has been added successfully!");
   } catch (error) {
-    print("error");
+    print("Error: $error");
   }
 }
 
@@ -74,17 +142,95 @@ deleteBook() {
   try {
     print("please inter book title:");
     String bookTitle = stdin.readLineSync()!;
+    bool flag = false;
     for (int i = 0; i <= myData.length - 1; i++) {
       if (myData[i]['bookTitle'] == bookTitle) {
         myData.removeWhere((item) => item["bookTitle"] == bookTitle);
         print("The book has been deleted successfully!");
-      } else if (i == myData.length - 1) {
+        flag = true;
+      } else if (i == myData.length - 1 && flag != true) {
         print(
             "The book does not exist! please check the spelling and try again.");
       }
     }
   } catch (error) {
-    print("error");
+    print("Error: $error");
+  }
+}
+
+searchByTitle() {
+  try {
+    print("please inter book title:");
+    String bookTitle = stdin.readLineSync()!;
+    bool flag = false;
+    for (int i = 0; i <= myData.length - 1; i++) {
+      if (myData[i]['bookTitle'] == bookTitle) {
+        print(myData[i]);
+        flag = true;
+      } else if (i == myData.length - 1 && flag != true) {
+        print(
+            "The book does not exist! please check the spelling and try again.");
+      }
+    }
+  } catch (error) {
+    print("Error: $error");
+  }
+}
+
+searchByAuthor() {
+  try {
+    print("please inter author name:");
+    String authorName = stdin.readLineSync()!;
+    bool flag = false;
+    for (int i = 0; i <= myData.length - 1; i++) {
+      if (myData[i]['authorName'] == authorName) {
+        print(myData[i]);
+        flag = true;
+      } else if (i == myData.length - 1 && flag != true) {
+        print(
+            "The author name does not exist! please check the spelling and try again.");
+      }
+    }
+  } catch (error) {
+    print("Error: $error");
+  }
+}
+
+searchByCategory() {
+  try {
+    print("please inter category:");
+    String category = stdin.readLineSync()!;
+    bool flag = false;
+    for (int i = 0; i <= myData.length - 1; i++) {
+      if (myData[i]['category'] == category) {
+        print(myData[i]);
+        flag = true;
+      } else if (i == myData.length - 1 && flag != true) {
+        print(
+            "The category does not exist! please check the spelling and try again.");
+      }
+    }
+  } catch (error) {
+    print("Error: $error");
+  }
+}
+
+searchInCategory(String category) {
+  ///for Extra creadt
+  try {
+    bool flag = false;
+    for (int i = 0; i <= myData.length - 1; i++) {
+      if (myData[i]['category'] == category) {
+        print(myData[i]);
+        flag = true;
+      } else if (i == myData.length - 1 && flag != true) {
+        print(
+            "The category does not exist! please check the spelling and try again.");
+        break;
+      }
+    }
+  } catch (error) {
+    print("Error: $error");
   }
 }
 
