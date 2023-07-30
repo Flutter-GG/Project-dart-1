e468ef15e984b82a8cb5aecabec6e7099261c272
Lama <84765301+Lama-Aldhafeeri@users.noreply.github.com>
2023-07-30 18:54:45 +0300

    Lama Aldhafeeri

--- project-1/bin/main.dart
+++ project-1/bin/main.dart
@@ -1,6 +1,5 @@
-import 'bookClass.dart';
+import 'classes.dart';
 import 'addEditDelFuns.dart';
-import 'basicFuns.dart';
 import 'purchaseFun.dart';
 import 'queryBooks.dart';
 import 'displayCategory.dart';
@@ -50,4 +49,62 @@ void main() {
         print("please select from the above menu (only by numbers)");
     }
   } while (true);
+}
+getDistinctTitles() {
+  Set titlesLocal = {};
+  // use Set to store distinct titles
+  for (var book in copyBooksData) {
+    titlesLocal.add(book["title"]);
+  }
+
+  return titlesLocal;
+}
+
+/// Lode data from 'data.dart' file to list using objects and then return the list
+loadDateToList() {
+  List<Book> booksListLocal = [];
+
+  for (var item in booksData) {
+    Book book = Book(
+        title: item["title"],
+        author: item["author"],
+        category: item["category"],
+        description: item["description"],
+        publicationYear: item["publicationYear"],
+        price: item["price"],
+        numberOfCopies: item["numberOfCopies"]);
+    booksListLocal.add(book);
+  }
+
+  return booksListLocal;
+}
+// to reduce repetition in the code
+/// this function to ask user to enter all required data from user
+queryFromUser({String? title}) {
+  //print("please enter book title");
+  //String title = stdin.readLineSync()!;
+  print("please enter book author");
+  String author = stdin.readLineSync()!;
+  print("please enter book category");
+  String category = stdin.readLineSync()!;
+  print("please enter book description");
+  String? description = stdin.readLineSync();
+  print("please enter publication year");
+  String? pubYear = stdin.readLineSync();
+  print("please enter book price");
+  int price = int.parse(stdin.readLineSync()!);
+  print("please enter number of copies available");
+  int numberOfCopies = int.parse(stdin.readLineSync()!);
+
+  final newBook = {
+    "title": title,
+    "author": author,
+    "category": category,
+    "description": description,
+    "publicationYear": pubYear,
+    "price": price,
+    "numberOfCopies": numberOfCopies
+  };
+
+  return newBook;
 }
